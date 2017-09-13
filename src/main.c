9e702ed0cdcf90ffb20a1c0386ec8c71099884f9
LINgene <much7770596@gmail.com>
2017-09-13 16:02:23 +0800

    first commit

---
+++ src/main.c
@@ -0,0 +1,28 @@
+
+#include "main.h"
+
+
+int main(void)
+{
+  
+  while (1)
+  {
+    
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
